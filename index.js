c3221896469077348738f778fb1d46277fa1bae3
Anton Shishenya <anton.shishenya@gmail.com>
2018-02-26 17:29:53 +0300

    Add /healthcheck endpoint

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 "use strict";
 
 const components = require("./components");
+var http = require("http");
 
 components.init().then(() => {
+    var config = components.config;
+    const server = http.createServer((req, res) => {
+        if (req.method === 'GET' && req.url === '/healthcheck') {
+            res.statusCode = 200;
+        } else {
+            res.statusCode = 404;
+        }
+        res.end();
+      }).listen(config.port);
+
     require("./server")();
 })
 .catch(err => {
